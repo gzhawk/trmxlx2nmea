c13f715cd6f626c5ba94e9384053ecfb1791cbf8
gzhawk <gzhawk@hotmail.com>
2022-09-07 17:27:39 +0800

    Update xlx2nmea.py
    fix the latitude and longtitude number format

--- xlx2nmea.py
+++ xlx2nmea.py
@@ -1,23 +1,17 @@
 """
 NMEA-0183 GGA (https://receiverhelp.trimble.com/alloy-gnss/en-us/NMEA-0183messages_GGA.html)
-
+               https://orolia.com/manuals/VSP/Content/NC_and_SS/Com/Topics/APPENDIX/NMEA_GGAmess.htm
 Field	Meaning
 0	    Message ID $GPGGA
-
 1	    UTC of position fix
-
 2	    Latitude
-
 3	    Direction of latitude:
         N: North
         S: South
-
 4	    Longitude
-
 5	    Direction of longitude:
         E: East
         W: West
-
 6	    GPS Quality indicator:
         0: Fix not valid
         1: GPS fix
@@ -26,21 +20,13 @@ Field	Meaning
         4: RTK Fixed, xFill
         5: RTK Float, OmniSTAR XP/HP, Location RTK, RTX
         6: INS Dead reckoning
-
 7	    Number of SVs in use, range from 00 through to 24+
-
 8	    HDOP
-
 9	    Orthometric height (MSL reference)
-
 10	    M: unit of measure for orthometric height is meters
-
 11	    Geoid separation
-
 12	    M: geoid separation measured in meters
-
 13	    Age of differential GPS data record, Type 1 or Type 9. Null field when DGPS is not used.
-
 14	    Reference station ID, range 0000 to 4095. 
         A null field when any reference station ID is selected and no corrections are received. 
         Reference Station ID	Service
@@ -58,7 +44,6 @@ Field	Meaning
         1016	                HP/XP (GPS)
         1020	                HP/G2 (GPS)
         1021	                HP/G2 (GPS/GLONASS) 
-
 15	    The checksum data, always begins with *
 """
 xver = '0.1'
@@ -68,7 +53,7 @@ import pynmea2
 
 xlx_path = 'C:\\Work\\Tools\\XLX2NMEA\\'
 #xlx_path = '/Users/Hawk/Downloads/'
-xlx_name = '22020831_sv_example'
+xlx_name = 'small_example'
 xlx_tail = '.xlsx'
 nmea_tail = '.txt'
 
@@ -88,20 +73,36 @@ nmea_ADGPS  = 20    #DGPS
 # Open a file for exclusive creation. If the file already exists, the operation fails.
 with open(xlx_path+xlx_name+nmea_tail, 'x') as nmea_log:
     for i in range(2, xlx_sht.max_row):
+        
+        La_data = xlx_sht.cell(i, nmea_La).value
+        if La_data > 0:
+            La_dir = 'N'
+        else:
+            La_dir = 'S'
+        La_data = int(La_data)*100+round((La_data - int(La_data))*60,3)
+
+        Lg_data = xlx_sht.cell(i, nmea_Lg).value
+        if  Lg_data > 0:
+            Lg_dir = 'E'
+        else:
+            Lg_dir = 'W'
+            Lg_data *= -1
+        Lg_data = int(Lg_data)*100+round((Lg_data - int(Lg_data))*60,3)
+
         msg = pynmea2.GGA('GP', 'GGA',
                           (str(xlx_sht.cell(i, nmea_UTC).value),
-                           str(xlx_sht.cell(i, nmea_La).value), 'N',
-                           str(xlx_sht.cell(i, nmea_Lg).value), 'E',
+                           str(La_data).zfill(4), La_dir,
+                           str(Lg_data).zfill(5), Lg_dir,
                            str(xlx_sht.cell(i, nmea_GQI).value),
-                           str(xlx_sht.cell(i, nmea_SV).value),
-                           str(xlx_sht.cell(i, nmea_HDOP).value),
-                           str(xlx_sht.cell(i, nmea_OH).value), 'M',
-                           '0', 'M',
+                           str(xlx_sht.cell(i, nmea_SV).value).zfill(2),
+                           str(round(xlx_sht.cell(i, nmea_HDOP).value,1)),
+                           str(round(xlx_sht.cell(i, nmea_OH).value,1)), 'M',
+                           '0.0', 'M',
                            str(xlx_sht.cell(i, nmea_ADGPS).value),
                            '0002'))
         nmea_log.write(str(msg))
         nmea_log.write('\n')
 
 print('Version:',xver)
-print('Successful convert XLX (',xlx_sht.max_row,'lines) into NMEA log:')
+print('Successful convert XLX (',xlx_sht.max_row-1,'lines) into NMEA log:')
 print(xlx_path+xlx_name+nmea_tail)
